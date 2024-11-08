6a796dafc782738dc5947401c56ff62bad531b1f
Nathan <nathanrogers1305@gmail.com>
2024-11-07 19:53:35 -0700

    Updates

---
+++ Assets/List.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class List : MonoBehaviour
+{
+
+    public List<string> pawns;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pawns = new List<string>();
+
+        pawns.Add(player.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/TankMovement.cs
+++ Assets/TankMovement.cs
@@ -10,28 +10,28 @@ public class TankMovement : MonoBehaviour
     public float speed;
     public float rotSpeed;
 
+    public KeyCode forward = KeyCode.W;
+    public KeyCode backward = KeyCode.S;
+    public KeyCode right = KeyCode.D;
+    public KeyCode left = KeyCode.A;
+
     // Update is called once per frame
     void Update()
     {
-         if (Input.GetKey(KeyCode.UpArrow))
-        {
-            //Move forwards
-            rb.velocity = transform.forward * speed;
-        }
+        //Move forwards and backwards
+            Vector3 movement = new Vector3(0f, 0f, Input.GetAxis("Vertical"));
+            if (Input.GetKey(forward) || Input.GetKey(backward))
+            {
+                transform.position += transform.rotation * movement * speed * Time.deltaTime;
+            }
 
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            //Move backwards
-            rb.velocity = -transform.forward * speed;
-        }
-
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(right))
         {
             //Rotate to the right
             transform.Rotate(new Vector3(0, rotSpeed, 0) * Time.deltaTime * speed, Space.World);
         }
 
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(left))
         {
             //Rotate to the left
             transform.Rotate(new Vector3(0, -rotSpeed, 0) * Time.deltaTime * speed, Space.World);
